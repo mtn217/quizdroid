f6350d021f93f5149af67f38ab8da389ab961634
Michael Nguyen <mtn217@uw.edu>
2017-01-31 02:31:06 -0800

    Started part2

---
+++ QuizDroidcurrent/app/src/main/java/edu/washington/mtn217/quizdroid/MarvelOverview.java
@@ -0,0 +1,24 @@
+package edu.washington.mtn217.quizdroid;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MarvelOverview extends Fragment {
+    public MarvelOverview() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_marvel_overview, container, false);
+    }
+}
---
+++ QuizDroidcurrent/app/src/main/java/edu/washington/mtn217/quizdroid/MathOverview.java
@@ -0,0 +1,29 @@
+package edu.washington.mtn217.quizdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MathOverview extends Fragment {
+    Button start;
+
+    public MathOverview() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_math_overview, container, false);
+    }
+
+}
