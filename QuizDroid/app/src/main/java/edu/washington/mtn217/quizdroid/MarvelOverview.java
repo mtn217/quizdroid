4df516b2122f15055a4891dd2d9a3ce194f4ac68
michael nguyen <mtn217@uw.edu>
2017-01-30 16:54:09 -0800

    Added first req

--- QuizDroid/app/src/main/java/edu/washington/mtn217/quizdroid/MarvelOverview.java
+++ QuizDroid/app/src/main/java/edu/washington/mtn217/quizdroid/MarvelOverview.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MarvelOverview extends AppCompatActivity {
 
+    Button startMarvel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marvel_overview);
+
+        startMarvel = (Button) findViewById(R.id.button10);
+        startMarvel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MarvelOverview.this, MarvelOne.class);
+                startActivity(intent);
+            }
+        });
     }
 }
