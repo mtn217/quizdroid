54f95d38c38a0a286202841efaff445b766d6ebc
Michael Nguyen <Michael Nguyen>
2017-01-30 17:19:36 -0800

    Added screenshots and set up layouts

--- QuizDroid/app/src/main/java/edu/washington/mtn217/quizdroid/MarvelOverview.java
+++ QuizDroid/app/src/main/java/edu/washington/mtn217/quizdroid/MarvelOverview.java
@@ -1,7 +1,10 @@
 package edu.washington.mtn217.quizdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MarvelOverview extends AppCompatActivity {
 
